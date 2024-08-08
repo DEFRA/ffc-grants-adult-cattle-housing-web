5656f16a35094c468b3fdb60bb09ebb8a612584a
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-08-08 16:02:48 +0100

    Provide local container root URL to appease pipeline tests

--- test/acceptance/wdio.conf.js
+++ test/acceptance/wdio.conf.js
@@ -90,7 +90,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: process.env.TEST_ENVIRONMENT_ROOT_URL,
+    baseUrl: process.env.TEST_ENVIRONMENT_ROOT_URL || "http://host.docker.internal:3600",
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
